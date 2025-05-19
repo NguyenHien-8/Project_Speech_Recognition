686ef4e47216b8d8305756ffb32f26f5003d917a
MrNGUYENHIEN <tnh200511@gmail.com>
2025-05-19 23:28:37 +0700

    Removed __pycache__ and .venv from Git tracking

--- Speech_VoskAPI.py
+++ Speech_VoskAPI.py
@@ -343,5 +343,5 @@ def get_latest_order():
         return {"Status": "No_Order", "Message": "No order has been placed yet."}
 
 if __name__ == "__main__":
-    uvicorn.run("DemoCode3:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("Speech_VoskAPI:app", host="0.0.0.0", port=8000, reload=False)
 
